49dc482c43d2c5c653541f4697b96c26191af775
sunilyadav1988 <sunilsit93@yahoo.com>
2018-07-10 23:47:34 +0530

    java program new1

--- java_program.java
+++ java_program.java
@@ -1,4 +1,4 @@
-public class HelloWorld_new {
+public class HelloWorld_new1 {
    public static void main(String[] args) {
       // Prints "Hello, World" in the terminal window.
       System.out.println("Hello, World");
