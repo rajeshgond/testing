c223534a57a6d01a6460c00888788db7a14efd2f
sunilyadav1988 <sunilsit93@yahoo.com>
2018-07-10 23:55:29 +0530

    java sample

--- java_program.java
+++ java_program.java
@@ -1,6 +1,5 @@
-public class HelloWorld_new1 {
-   public static void main(String[] args) {
-      // Prints "Hello, World" in the terminal window.
-      System.out.println("Hello, World");
-   }
-}
+class Simple{  
+    public static void main(String args[]){  
+     System.out.println("Hello Java");  
+    }  
+}  
